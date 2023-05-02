b9de221f8c8f572c246677e70fa24634466c1ac0
Bruce Collie <brucecollie82@gmail.com>
2023-05-02 12:02:48 +0200

    Import existing tutorial definition of IMP (#71)
    * Import existing tutorial definition of IMP
    
    * Strip out old text
    
    * Add trivial example
    
    * Start on rough KIMP infra
    
    * Rough runner
    
    * Check for stuck config
    
    * Set up test harness

--- k-src/imp-pldi.md
+++
@@ -1,7 +0,0 @@
-```k
-require "imp-pldi-syntax.md"
-
-module IMP-PLDI
-  imports IMP-PLDI-SYNTAX
-endmodule
-```
--- src/tests/integration/test_integration.py
+++ src/tests/integration/test_integration.py
@@ -1,2 +1,22 @@
-def test_integration() -> None:
-    pass
+from pathlib import Path
+
+import pytest
+
+from kimp.kimp import KIMP
+
+IMP_EXAMPLES_DIR = Path(__file__).parent.parent.parent.parent / 'examples' / 'imp'
+
+IMP_TEST_DATA = [(path, path.stem) for path in IMP_EXAMPLES_DIR.glob('*.imp')]
+
+
+@pytest.mark.parametrize(
+    ('input_path', 'test_id'),
+    IMP_TEST_DATA,
+    ids=[test_id for _, test_id in IMP_TEST_DATA],
+)
+def test_integration(kimp: KIMP, input_path: Path, test_id: str) -> None:
+    result = kimp.run_imp_file(input_path)
+    rc, errs = kimp.postprocess(result)
+
+    assert rc == 0
+    assert len(errs) == 0
